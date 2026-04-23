adda501e4b1d69f42b0612981ea97d0caa1383d5
pa001024 <pa001024@qq.com>
2026-04-23 14:35:58 +0800

    cc: IdentifyTime

--- Script/Datas/ExtractionTreasureRarity.lua
+++ Script/Datas/ExtractionTreasureRarity.lua
@@ -6,32 +6,32 @@ local ReadOnly = (DataMgr or {}).ReadOnly or function(n, x)
 end
 return ReadOnly("ExtractionTreasureRarity", {
   [1] = {
-    IdentifyTime = 1,
+    IdentifyTime = 0.1,
     ShowRarity = 2,
     TreasureRarity = 1
   },
   [2] = {
-    IdentifyTime = 1.5,
+    IdentifyTime = 0.1,
     ShowRarity = 4,
     TreasureRarity = 2
   },
   [3] = {
-    IdentifyTime = 2,
+    IdentifyTime = 0.1,
     ShowRarity = 5,
     TreasureRarity = 3
   },
   [4] = {
-    IdentifyTime = 2.5,
+    IdentifyTime = 0.1,
     ShowRarity = 6,
     TreasureRarity = 4
   },
   [5] = {
-    IdentifyTime = 3,
+    IdentifyTime = 0.1,
     ShowRarity = 6,
     TreasureRarity = 5
   },
   [6] = {
-    IdentifyTime = 2,
+    IdentifyTime = 0.1,
     ShowRarity = 5,
     TreasureRarity = 6
   }
